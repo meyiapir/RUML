b6db59545ae0ddadbdd2d54fd0cfaddf9209d7f5
meyap <mmeyiapir@gmail.com>
2023-09-10 10:40:28 +0300

    First

---
+++ app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/predict/")
+def get_video_id(user_id: str, num_videos: int):
+    # Здесь можно добавить логику для получения предзаготовленного значения "video_id"
+    video_ids = "call func"
+
+    return {"predictions": video_ids}
